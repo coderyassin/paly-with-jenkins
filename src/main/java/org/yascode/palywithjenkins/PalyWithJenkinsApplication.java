8c076d1a3275e952bbbf79414ceee0206371202d
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2023-01-20 22:54:58 +0100

    first commit

---
+++ src/main/java/org/yascode/palywithjenkins/PalyWithJenkinsApplication.java
@@ -0,0 +1,13 @@
+package org.yascode.palywithjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalyWithJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalyWithJenkinsApplication.class, args);
+	}
+
+}
